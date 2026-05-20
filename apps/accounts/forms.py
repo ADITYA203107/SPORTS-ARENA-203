2fdd3ea6ea9aa10fcfede6a5b8909b02ca15abf9
ADITYA KUMAR <kumar.adityasingh1000@gmail.com>
2026-05-21 04:33:27 +0530

    Heyy boy

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
+INPUT_CLASS = 'w-full bg-white border border-slate-300 rounded-xl px-4 py-3 text-slate-900 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all'
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     role = forms.ChoiceField(
@@ -14,25 +16,22 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'role')
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+            'username': forms.TextInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Email'}),
         }
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
-        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
-        
-        # Remove help text for cleaner UI
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Password'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Confirm Password'})
+        self.fields['email'].widget = forms.EmailInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Email'})
         for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
 
 class CustomAuthenticationForm(forms.Form):
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
+        widget=forms.EmailInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Email address'})
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+        widget=forms.PasswordInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Password'})
     )
